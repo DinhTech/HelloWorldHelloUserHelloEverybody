136b1bb979fa14c60acb50052fe2eff6135b300e
DinhTech <67037099+DinhTech@users.noreply.github.com>
2020-06-21 20:22:46 -0600

    fixed bug #001

---
+++ HelloWorldUserEB/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorldUserEB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+
+            }
+        }
+    }
+}
